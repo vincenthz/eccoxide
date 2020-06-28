196efcedd666356fe238f43e4d2652468ba79894
Vincent Hanquez <vincent@typed.io>
2020-06-28 12:37:26 +0800

    define all prime curves

--- src/curve/sec2.rs
+++ src/curve/sec2.rs
@@ -1,12 +1,32 @@
-pub mod p256k1 {
-    use super::super::helper::mod_inverse;
-    use crate::params::sec2::p256k1::*;
-    use crate::scalar_impl;
-    use lazy_static;
-    use num_bigint::BigUint;
+macro_rules! prime_curve {
+    ($m: ident) => {
+        pub mod $m {
+            use super::super::helper::mod_inverse;
+            use crate::params::sec2::$m::*;
+            use crate::scalar_impl;
+            use lazy_static;
+            use num_bigint::BigUint;
 
-    lazy_static! {
-        static ref P: BigUint = BigUint::from_bytes_be(&P_BYTES);
-    }
-    scalar_impl!(&*P);
+            lazy_static! {
+                static ref P: BigUint = BigUint::from_bytes_be(&P_BYTES);
+            }
+            scalar_impl!(&*P);
+        }
+    };
 }
+
+prime_curve!(p112r1);
+prime_curve!(p112r2);
+prime_curve!(p128r1);
+prime_curve!(p128r2);
+prime_curve!(p160k1);
+prime_curve!(p160r1);
+prime_curve!(p160r2);
+prime_curve!(p192k1);
+prime_curve!(p192r1);
+prime_curve!(p224k1);
+prime_curve!(p224r1);
+prime_curve!(p256k1);
+prime_curve!(p256r1);
+prime_curve!(p384r1);
+prime_curve!(p521r1);
