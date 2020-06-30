6ba97726608507c258d1fbabe32964600b2ce5aa
Vincent Hanquez <vincent@typed.io>
2020-06-30 08:34:40 +0800

    implement more point addition, some KATs, arithmetic tests

--- src/curve/macros.rs
+++ src/curve/macros.rs
@@ -2,12 +2,22 @@
 #[macro_export]
 macro_rules! scalar_impl {
     ($p: expr, $sz: expr) => {
-        #[derive(Debug, Clone)]
+        #[derive(Clone)]
         pub struct Scalar(num_bigint::BigUint);
 
         impl PartialEq for Scalar {
             fn eq(&self, other: &Self) -> bool {
                 &self.0 == &other.0
+            }
+        }
+
+        impl std::fmt::Debug for Scalar {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                let bs = self.0.to_bytes_be();
+                for b in bs.iter() {
+                    write!(f, "{:02x}", b)?
+                }
+                Ok(())
             }
         }
 
@@ -30,11 +40,6 @@ macro_rules! scalar_impl {
             pub fn add_assign(&mut self, other: &Self) {
                 self.0 += &other.0;
                 self.0 %= $p;
-            }
-
-            /// Negate the scalar
-            pub fn negate(&self) -> Self {
-                Scalar(($p - &self.0) % $p)
             }
 
             /// Get the multiplicative inverse
@@ -74,6 +79,22 @@ macro_rules! scalar_impl {
                 let bs = self.0.to_bytes_be();
                 out[start..].copy_from_slice(&bs);
                 out
+            }
+        }
+
+        impl std::ops::Neg for Scalar {
+            type Output = Scalar;
+
+            fn neg(self) -> Self::Output {
+                Scalar($p - self.0)
+            }
+        }
+
+        impl std::ops::Neg for &Scalar {
+            type Output = Scalar;
+
+            fn neg(self) -> Self::Output {
+                Scalar($p - &self.0)
             }
         }
 
@@ -121,7 +142,7 @@ macro_rules! scalar_impl {
             type Output = Scalar;
 
             fn sub(self, other: &'b Scalar) -> Scalar {
-                Scalar((&self.0 + &other.negate().0) % $p)
+                Scalar((&self.0 + (-other).0) % $p)
             }
         }
 
@@ -192,7 +213,7 @@ macro_rules! scalar_impl {
 macro_rules! point_impl {
     ($gx: expr, $gy: expr) => {
         /// Affine Point on the curve
-        #[derive(Clone, Debug)]
+        #[derive(Clone, Debug, PartialEq, Eq)]
         pub struct PointAffine {
             x: Scalar,
             y: Scalar,
@@ -206,9 +227,19 @@ macro_rules! point_impl {
             z: Scalar,
         }
 
+        impl PartialEq for Point {
+            fn eq(&self, other: &Point) -> bool {
+                self.to_affine() == other.to_affine()
+            }
+        }
+
+        impl Eq for &Point {}
+
         lazy_static! {
             static ref A: Scalar = Scalar(BigUint::from_bytes_be(&A_BYTES));
             static ref B: Scalar = Scalar(BigUint::from_bytes_be(&B_BYTES));
+            static ref B3: Scalar =
+                Scalar(BigUint::from_bytes_be(&B_BYTES) * BigUint::from_bytes_be(&[3]));
             static ref GX: Scalar = Scalar(BigUint::from_bytes_be(&GX_BYTES));
             static ref GY: Scalar = Scalar(BigUint::from_bytes_be(&GY_BYTES));
         }
@@ -225,7 +256,7 @@ macro_rules! point_impl {
             // check if y^2 = x^3 + a*x + b (mod p) holds
             pub fn from_coordinate(x: &Scalar, y: &Scalar) -> Option<Self> {
                 let y2 = y * y;
-                let x3 = x * x * x;
+                let x3 = x.power(3);
                 let ax = &*A * x;
 
                 if y2 == x3 + ax + &*B {
@@ -304,8 +335,26 @@ macro_rules! point_impl {
         impl<'a, 'b> std::ops::Mul<&'b Scalar> for &'a Point {
             type Output = Point;
 
-            fn mul(self, _other: &'b Scalar) -> Point {
-                //
+            fn mul(self, other: &'b Scalar) -> Point {
+                /*
+                let mut p1 = self.clone();
+                let mut p2 = self.double();
+                let n = other.clone();
+                let b = BigUInt::from(1);
+                let mut bits = n.bits();
+                while bits {
+                    if n & b == b {
+                        p1 = &p1 + &p1;
+                        p2 = &p1 + p2;
+                    } else {
+                        p1 = p1 + &p2;
+                        p2 = &p2 + &p2;
+                    }
+                    n >>= 1;
+                    bits -= 1;
+                }
+                p1
+                */
                 todo!()
             }
         }
@@ -317,10 +366,151 @@ macro_rules! point_impl {
         impl<'a, 'b> std::ops::Add<&'b Point> for &'a Point {
             type Output = Point;
 
-            fn add(self, _other: &'b Point) -> Point {
+            fn add(self, other: &'b Point) -> Point {
+                let Point {
+                    x: ref x1,
+                    y: ref y1,
+                    z: ref z1,
+                } = &self;
+                let Point {
+                    x: ref x2,
+                    y: ref y2,
+                    z: ref z2,
+                } = &other;
+
+                // https://eprint.iacr.org/2015/1060.pdf
+                // Algorithm 1. Complete, projective point addition for arbitrary
+                // prime order short weirstrass curves E/Fq : y^2 = x^3 + ax + b
                 //
-                todo!()
+                //  1. t0 ← X1 · X2     2. t1 ← Y1 · Y2     3. t2 ← Z1 · Z2
+                //  4. t3 ← X1 + Y1     5. t4 ← X2 + Y2     6. t3 ← t3 · t4
+                //  7. t4 ← t0 + t1     8. t3 ← t3 − t4     9. t4 ← X1 + Z1
+                // 10. t5 ← X2 + Z2    11. t4 ← t4 · t5    12. t5 ← t0 + t2
+                // 13. t4 ← t4 − t5    14. t5 ← Y1 + Z1    15. X3 ← Y2 + Z2
+                // 16. t5 ← t5 · X3    17. X3 ← t1 + t2    18. t5 ← t5 − X3
+                // 19. Z3 ← a · t4     20. X3 ← b3 · t2    21. Z3 ← X3 + Z3
+                // 22. X3 ← t1 − Z3    23. Z3 ← t1 + Z3    24. Y3 ← X3 · Z3
+                // 25. t1 ← t0 + t0    26. t1 ← t1 + t0    27. t2 ← a · t2
+                // 28. t4 ← b3 · t4    29. t1 ← t1 + t2    30. t2 ← t0 − t2
+                // 31. t2 ← a · t2     32. t4 ← t4 + t2    33. t0 ← t1 · t4
+                // 34. Y3 ← Y3 + t0    35. t0 ← t5 · t4    36. X3 ← t3 · X3
+                // 37. X3 ← X3 − t0    38. t0 ← t3 · t1    39. Z3 ← t5 · Z3
+                // 40. Z3 ← Z3 + t0
+
+                let t0 = x1 * x2;
+                let t1 = y1 * y2;
+                let t2 = z1 * z2;
+                let t3 = x1 + y1;
+                let t4 = x2 + y2;
+                let t3 = t3 * t4;
+                let t4 = &t0 + &t1;
+                let t3 = t3 - &t4;
+                let t4 = x1 + z1;
+                let t5 = x2 + z2;
+                let t4 = t4 * &t5;
+                let t5 = &t0 + &t2;
+                let t4 = t4 - &t5;
+                let t5 = y1 + z1;
+                let x3 = y2 + z2;
+                let t5 = t5 * &x3;
+                let x3 = &t1 + &t2;
+                let t5 = t5 - &x3;
+                let z3 = &*A * &t4;
+                let x3 = &*B3 * &t2;
+                let z3 = &x3 + z3;
+                let x3 = &t1 - &z3;
+                let z3 = &t1 + &z3;
+                let y3 = &x3 * &z3;
+                let t1 = t0.double();
+                let t1 = t1 + &t0;
+                let t2 = &*A * t2;
+                let t4 = &*B3 * &t4;
+                let t1 = t1 + &t2;
+                let t2 = &t0 - t2;
+                let t2 = &*A * t2;
+                let t4 = t4 + &t2;
+                let t0 = &t1 * &t4;
+                let y3 = y3 + t0;
+                let t0 = &t5 * &t4;
+                let x3 = &t3 * x3;
+                let x3 = x3 - &t0;
+                let t0 = &t3 * &t1;
+                let z3 = &t5 * &z3;
+                let z3 = z3 + t0;
+
+                Point {
+                    x: x3,
+                    y: y3,
+                    z: z3,
+                }
             }
+        }
+
+        impl<'b> std::ops::Add<&'b Point> for Point {
+            type Output = Point;
+
+            fn add(self, other: &'b Point) -> Point {
+                &self + other
+            }
+        }
+
+        impl<'a> std::ops::Add<Point> for &'a Point {
+            type Output = Point;
+
+            fn add(self, other: Point) -> Point {
+                self + &other
+            }
+        }
+
+        impl std::ops::Add<Point> for Point {
+            type Output = Point;
+
+            fn add(self, other: Point) -> Point {
+                &self + &other
+            }
+        }
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! test_scalar_arithmetic {
+    () => {
+        #[test]
+        fn scalar_negate() {
+            assert_eq!(Scalar::one() + (-Scalar::one()), Scalar::zero())
+        }
+
+        #[test]
+        fn scalar_inverse() {
+            assert_eq!(
+                Scalar::one() * Scalar::one().inverse().unwrap(),
+                Scalar::one()
+            );
+
+            let mut v = Scalar::one() + Scalar::one();
+            for _ in 0..15 {
+                assert_eq!(&v * v.inverse().unwrap(), Scalar::one());
+                v = v + Scalar::one();
+            }
+        }
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! test_point_arithmetic {
+    () => {
+        #[test]
+        fn point_add_infinity() {
+            assert_eq!(Point::generator() + Point::infinity(), Point::generator())
+        }
+
+        fn point_affine_projective() {
+            assert_eq!(
+                Point::from(Point::generator().to_affine().unwrap()),
+                Point::generator()
+            )
         }
     };
 }
--- src/curve/sec2.rs
+++ src/curve/sec2.rs
@@ -13,6 +13,15 @@ macro_rules! prime_curve {
             }
             scalar_impl!(&*P, $szfe);
             point_impl!(&*GX, &*GY);
+
+            #[cfg(test)]
+            mod tests {
+                use super::*;
+                use crate::{test_point_arithmetic, test_scalar_arithmetic};
+
+                test_scalar_arithmetic!();
+                test_point_arithmetic!();
+            }
         }
     };
 }
@@ -32,3 +41,83 @@ prime_curve!(p256k1, 32);
 prime_curve!(p256r1, 32);
 prime_curve!(p384r1, 48);
 prime_curve!(p521r1, 66);
+
+#[cfg(test)]
+mod tests {
+    mod p256r1 {
+        use super::super::p256r1::*;
+
+        #[test]
+        fn point_add() {
+            let k2_x = b"\x7C\xF2\x7B\x18\x8D\x03\x4F\x7E\x8A\x52\x38\x03\x04\xB5\x1A\xC3\xC0\x89\x69\xE2\x77\xF2\x1B\x35\xA6\x0B\x48\xFC\x47\x66\x99\x78";
+            let k2_y = b"\x07\x77\x55\x10\xDB\x8E\xD0\x40\x29\x3D\x9A\xC6\x9F\x74\x30\xDB\xBA\x7D\xAD\xE6\x3C\xE9\x82\x29\x9E\x04\xB7\x9D\x22\x78\x73\xD1";
+            let x = Scalar::from_bytes(k2_x).unwrap();
+            let y = Scalar::from_bytes(k2_y).unwrap();
+
+            let p_expected = PointAffine::from_coordinate(&x, &y).unwrap();
+            let p_got = &Point::generator() + &Point::generator();
+            let p_got_affine = p_got.to_affine().unwrap();
+            let p_got_affine = PointAffine::generator();
+
+            assert_eq!(p_expected, p_got_affine);
+            //
+        }
+    }
+
+    mod p192r1 {
+        use super::super::p192r1::*;
+
+        #[test]
+        fn point_add() {
+            const KAT0: (&[u8], &[u8]) = (
+                &[
+                    0x18, 0x8d, 0xa8, 0x0e, 0xb0, 0x30, 0x90, 0xf6, 0x7c, 0xbf, 0x20, 0xeb, 0x43,
+                    0xa1, 0x88, 0x00, 0xf4, 0xff, 0x0a, 0xfd, 0x82, 0xff, 0x10, 0x12,
+                ],
+                &[
+                    0x07, 0x19, 0x2b, 0x95, 0xff, 0xc8, 0xda, 0x78, 0x63, 0x10, 0x11, 0xed, 0x6b,
+                    0x24, 0xcd, 0xd5, 0x73, 0xf9, 0x77, 0xa1, 0x1e, 0x79, 0x48, 0x11,
+                ],
+            );
+
+            const KAT1: (&[u8], &[u8]) = (
+                &[
+                    0xda, 0xfe, 0xbf, 0x58, 0x28, 0x78, 0x3f, 0x2a, 0xd3, 0x55, 0x34, 0x63, 0x15,
+                    0x88, 0xa3, 0xf6, 0x29, 0xa7, 0x0f, 0xb1, 0x69, 0x82, 0xa8, 0x88,
+                ],
+                &[
+                    0xdd, 0x6b, 0xda, 0x0d, 0x99, 0x3d, 0xa0, 0xfa, 0x46, 0xb2, 0x7b, 0xbc, 0x14,
+                    0x1b, 0x86, 0x8f, 0x59, 0x33, 0x1a, 0xfa, 0x5c, 0x7e, 0x93, 0xab,
+                ],
+            );
+            const KAT2: (&[u8], &[u8]) = (
+                &[
+                    0x76, 0xe3, 0x2a, 0x25, 0x57, 0x59, 0x9e, 0x6e, 0xdc, 0xd2, 0x83, 0x20, 0x1f,
+                    0xb2, 0xb9, 0xaa, 0xdf, 0xd0, 0xd3, 0x59, 0xcb, 0xb2, 0x63, 0xda,
+                ],
+                &[
+                    0x78, 0x2c, 0x37, 0xe3, 0x72, 0xba, 0x45, 0x20, 0xaa, 0x62, 0xe0, 0xfe, 0xd1,
+                    0x21, 0xd4, 0x9e, 0xf3, 0xb5, 0x43, 0x66, 0x0c, 0xfd, 0x05, 0xfd,
+                ],
+            );
+
+            let kats = [KAT0, KAT1, KAT2];
+
+            let mut points = Vec::new();
+            for k in &kats {
+                let x = Scalar::from_bytes(k.0).unwrap();
+                let y = Scalar::from_bytes(k.1).unwrap();
+
+                let p_expected = PointAffine::from_coordinate(&x, &y)
+                    .expect("cannot convert from affine: test vector value failed");
+                points.push(p_expected);
+            }
+
+            let p = PointAffine::generator();
+            assert_eq!(p, points[0]);
+
+            let p = Point::generator() + Point::generator();
+            assert_eq!(p.to_affine().unwrap(), points[1]);
+        }
+    }
+}
