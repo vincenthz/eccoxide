236e0259cab1bfa9383cf2e4bdb467232235ab80
Vincent Hanquez <vincent@typed.io>
2020-09-23 08:08:42 +0800

    add p256r1, p384r1, p521r1 fiat-crypto generated modules

--- src/curve/fiat/mod.rs
+++ src/curve/fiat/mod.rs
@@ -1,5 +1,12 @@
 #![allow(dead_code)]
 #![allow(non_camel_case_types)]
+
+pub mod p256_64;
+pub mod p256_scalar_64;
+pub mod p384_64;
+pub mod p384_scalar_64;
+pub mod p521_64;
+pub mod p521_scalar_64;
 pub mod secp256k1_64;
 pub mod secp256k1_scalar_64;
 
