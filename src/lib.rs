8f1f6f97d8ada37187b44cc606a119b057d1762f
Vincent Hanquez <vincent@typed.io>
2021-10-25 12:05:49 +0800

    add one more example

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,32 @@
 //! let point = generator.to_affine().unwrap();
 //! let (x, y) = point.to_coordinate();
 //! ```
+//!
+//! Using
+//!
+//! ```
+//! // use p521r1 for this example
+//! use eccoxide::curve::sec2::p521r1::{FieldElement, Scalar, Point};
+//! use eccoxide::curve::Sign;
+//!
+//! // this is just unsecure example, but here it could be loading some secret from disk
+//! let bytes : [u8;66] = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+//!
+//! let secret_key = Scalar::from_bytes(&bytes).unwrap();
+//! let public_key = &Point::generator() * &secret_key;
+//!
+//! // serialize the public key to a standard-ish compress format for p521r1
+//! let public_affine = public_key.to_affine().unwrap();
+//! let (x, ysign) = public_affine.compress();
+//! let format_byte : u8 = match ysign {
+//!     Sign::Positive => 0x2,
+//!     Sign::Negative => 0x3,
+//! };
+//!
+//! let mut public_key_bytes = Vec::new();
+//! public_key_bytes.push(format_byte);
+//! public_key_bytes.extend_from_slice(&x.to_bytes());
+//! ```
 
 #[macro_use]
 extern crate lazy_static;
